153266e8c0129b0de742cac8bce9bfd9f08f4be1
Naveen Kendyala <subscribeken@gmail.com>
2023-11-14 12:03:53 -0500

    First Draft

---
+++ 00.demo.sh
@@ -0,0 +1,39 @@
+# Install the Operator
+
+# Create a project amq-broker-demo
+oc new-project amq-broker-demo
+
+# Sample Broker 
+apiVersion: broker.amq.io/v1beta1
+kind: ActiveMQArtemis
+metadata:
+  name: ex-aao
+spec:
+  deploymentPlan:
+    # Number of Broker Instances
+    size: 2
+    # Broker Image
+    image: placeholder
+    requireLogin: false
+    persistenceEnabled: true
+    journalType: nio
+    # Applies only when the number of broker pods are reduced and has atleast one pod running
+    messageMigration: true
+    initImage: placeholder    
+    # Provide Storage that is applicable for all the broker instances
+    storage:
+      size: 4Gi
+      storageClassName: gp2-csi
+    # CPU, Memory Resources & Limits used by the Broker Instance  
+    resources:
+      limits:
+        cpu: "500m"
+        memory: "1024M"
+      requests:
+        cpu: "250m"
+        memory: "512M"
+  # Enabling Broker console access
+  console:
+    expose: true        
+
+# 
